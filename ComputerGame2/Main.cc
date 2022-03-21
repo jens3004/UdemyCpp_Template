9b63537645b93528592ca80b5e3645432d01ab44
jens3004 <fricke721@gmail.com>
2022-03-21 15:18:51 +0100

    Stand 2022-03-21

---
+++ ComputerGame2/Main.cc
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "Game.h"
+
+int main()
+{
+    bool repeat = true;
+
+    while (repeat)
+    {
+        game();
+
+        std::cout << "Play again? (0=No, 1=Yes)" << std::endl;
+        std::cin >> repeat;
+
+        system("clear");
+    }
+
+    return 0;
+}
