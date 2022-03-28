86602e9449a2208e4fe119d8a424d7b974aa4914
jens3004 <fricke721@gmail.com>
2022-03-28 15:52:07 +0200

    Stand 2022-03-28

---
+++ CodingProject1/AlgoArray.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "DynArray.h"
+
+/**
+ * @brief
+ *
+ * @param dynamic_array
+ * @return double
+ */
+double sum(const DynamicArray &dynamic_array);
+
+/**
+ * @brief
+ *
+ * @param dynamic_array
+ * @return double
+ */
+double mean(const DynamicArray &dynamic_array);
+
+/**
+ * @brief
+ *
+ * @param dynamic_array
+ * @return double
+ */
+double median(const DynamicArray &dynamic_array);
+
+/**
+ * @brief
+ *
+ * @param dynamic_array
+ * @return double
+ */
+double variance(const DynamicArray &dynamic_array);
+
+/**
+ * @brief
+ *
+ * @param dynamic_array
+ * @return double
+ */
+double stddev(DynamicArray &dynamic_array);
